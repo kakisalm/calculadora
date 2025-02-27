37da923e98f6fbf7a3a6c1b15c8171c23a215583
jonatasoliveiradasilva <oliveirajonatas@outlook.com.br>
2025-02-27 17:21:43 -0300

    calculadora (adição e subtração)

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Digite o primeiro número:");
+            int primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            int segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o número correspondente à operação: (1 - adição, 2 - subtração)");
+            int operacao = Convert.ToInt32(Console.ReadLine());
+
+            switch (operacao)
+            {
+                case 1:
+                    Console.WriteLine(primeiroNumero + segundoNumero);
+                    break;
+                case 2:
+                    Console.WriteLine(primeiroNumero - segundoNumero);
+                    break;
+                default:
+                    Console.WriteLine("Operação inválida. Tente novamente.");
+                    break;
+            }
         }
     }
 }
