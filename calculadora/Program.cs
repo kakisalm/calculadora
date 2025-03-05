598d9ed3093f5c1dbc5acb415d9b18564cfb4ad5
kakisalm <karyne.abalmeida@senacsp.edu.br>
2025-03-04 23:18:30 -0300

    program.cs
    master, junção das 4 operações matemáticas

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 class Calculadora
 {
@@ -10,52 +10,42 @@ class Calculadora
         Console.Write("Digite o segundo número: ");
         double num2 = Convert.ToDouble(Console.ReadLine());
 
-
         Console.WriteLine("\nQual operação você deseja realizar?");
         Console.WriteLine("Digite 5 para Multiplicação");
         Console.WriteLine("Digite 6 para Divisão");
+        Console.WriteLine("Digite 1 para Adição");
+        Console.WriteLine("Digite 2 para Subtração");
 
         int escolha = Convert.ToInt32(Console.ReadLine());
 
         switch (escolha)
         {
-            Console.WriteLine("Digite o primeiro número:");
-            int primeiroNumero = Convert.ToInt32(Console.ReadLine());
+            case 1:
+                Console.WriteLine("Soma: " + (num1 + num2));
+                break;
+                
+            case 2:
+                Console.WriteLine("Subtração: " + (num1 - num2));
+                break;
 
-            Console.WriteLine("Digite o segundo número:");
-            int segundoNumero = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Digite o número correspondente à operação: (1 - adição, 2 - subtração)");
-            int operacao = Convert.ToInt32(Console.ReadLine());
-
-            switch (operacao)
-            {
-                case 1:
-                    Console.WriteLine(primeiroNumero + segundoNumero);
-                    break;
-                case 2:
-                    Console.WriteLine(primeiroNumero - segundoNumero);
-                    break;
-                default:
-                    Console.WriteLine("Operação inválida. Tente novamente.");
-                    break;
-            }
-        }
-    }
             case 5:
                 Console.WriteLine("Multiplicação: " + (num1 * num2));
                 break;
 
             case 6:
-                Console.WriteLine("Divisão: " + (num1 / num2));
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Erro: Não é possível dividir por zero.");
+                }
+                else
+                {
+                    Console.WriteLine("Divisão: " + (num1 / num2));
+                }
                 break;
 
             default:
-                Console.WriteLine("Escolha inválida! Por favor, insira um número entre 5 e 6.");
+                Console.WriteLine("Escolha inválida! Por favor, insira um número entre 1, 2, 5 e 6.");
                 break;
         }
     }
-
-
- master
 }
